b20230eeb226e344d1e19f58414503e30d5ee19c
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2018-02-04 22:07:38 -0200

    Arrumando url do bancho

--- src/class/Config.js
+++ src/class/Config.js
@@ -8,8 +8,7 @@ class Config {
 	}
 	
 	static get database () {
-		// return 'mongodb://localhost:27017/estudo';
-		return 'mongodb://adotapets:admin@cluster0-shard-00-00-h6bbx.mongodb.net:27017,cluster0-shard-00-01-h6bbx.mongodb.net:27017,cluster0-shard-00-02-h6bbx.mongodb.net:27017/adotapets?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+		return 'mongodb://localhost:27017/churras';
 	}
 
 	static get allowOrigin () {
